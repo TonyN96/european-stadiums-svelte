6a91ce46ea94838d78e356cb11880ecbce326317
Tony Naughton <tonynaughton1@gmail.com>
2021-05-15 16:07:56 +0100

    Login component and view implemented

---
+++ src/pages/Login.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { navBar, welcomeMenu } from "../stores";
+    import LoginForm from "../components/LoginForm.svelte";
+
+    navBar.set({
+        bar: welcomeMenu,
+    });
+</script>
+
+<div class="uk-container uk-margin uk-margin-top">
+    <div
+        class="uk-margin-top uk-child-width-expand uk-flex-center uk-flex-middle uk-text-center"
+        uk-grid
+    >
+        <div>
+            <div>
+                <h3 class="uk-text-center">Log in</h3>
+                <LoginForm />
+            </div>
+        </div>
+        <div>
+            <img
+                src="https://res.cloudinary.com/dczqccpne/image/upload/v1615905074/c_383_bfdcd56c-4f2d-4263-bb4f-0a540fcb8bc6_rwwpd0.jpg"
+                alt="login-img"
+            />
+        </div>
+    </div>
+</div>
---
+++ src/pages/Main.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { navBar, welcomeMenu } from "../stores";
+    navBar.set({
+        bar: welcomeMenu,
+    });
+</script>
+
+<div class="uk-container uk-margin">
+    <div class="uk-width-expand uk-flex-center uk-flex-middle uk-text-center uk-margin-top" uk-grid>
+        <div class="uk-width-expand">
+            <img
+                width="500"
+                src="https://res.cloudinary.com/dczqccpne/image/upload/v1615904950/stadium_vrzmkh.jpg"
+                alt="main-img"
+            />
+        </div>
+        <div class="uk-width-expand">
+            <h1 class="uk-heading-small">Welcome to European Stadiums</h1>
+            <p>
+                <a href="/#/signup">Sign Up</a> or <a href="/#/login">Log in</a> to view and edit stadiums.
+            </p>
+        </div>
+    </div>
+</div>
